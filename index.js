78fcfb0e921de1865029aa5d5d88e261c58fe00e
keeganosler <oslerkeegan@gmail.com>
2023-05-27 15:05:29 -0600

    add rounding

--- index.js
+++ index.js
@@ -10,8 +10,7 @@ income2.addEventListener("input", computePortions)
 function computePortions() {
     if (amount.value && income1.value && income2.value) {
         let total = (+income1.value) + (+income2.value);
-        document.getElementById('portion1').innerHTML = '$' + ((income1.value / total) * amount.value).toString();
-        document.getElementById('portion2').innerHTML = '$' + ((income2.value / total) * amount.value).toString();        
+        document.getElementById('portion1').innerHTML = '$' + ((income1.value / total) * amount.value).toFixed(2).toString();
+        document.getElementById('portion2').innerHTML = '$' + ((income2.value / total) * amount.value).toFixed(2).toString();        
     }
-
 }
